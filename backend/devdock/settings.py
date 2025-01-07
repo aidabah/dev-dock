bd5080bddfe14addcb8e582183f7b9cb0b60d39c
Aida Bah <aidabah1975@gmail.com>
2025-01-06 20:11:31 -0600

    Configure urls for tasks app

--- backend/devdock/settings.py
+++ backend/devdock/settings.py
@@ -36,10 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
-    'core'
-    'projects'
-    'tasks'
+    'django.contrib.staticfiles',
+    'core',
+    'projects',
+    'tasks',
     'users',
 ]
 
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def main(request):
+    return HttpResponse('''<h1>Welcome to DevDock!</h1>
+                           <h2>These are your tasks.</h2>
+                        ''')
+
